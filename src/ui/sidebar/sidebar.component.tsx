c495db4e12f9dab29dfe741a3ea7067de1fb753a
Akoz <jedealbagaytan@gmail.com>
2023-11-05 21:57:03 -0700

    optmized animation

--- src/ui/sidebar/sidebar.component.tsx
+++ src/ui/sidebar/sidebar.component.tsx
@@ -4,7 +4,7 @@ export const SideBar = () => {
   return (
     <aside className={styles.sidebar}>
       <header className=''>
-        <img src='/logos/deluxe.png' alt='Logo de la empresa' width={3543} height={3543} />
+        <img src='/logos/deluxe.png' alt='Logo de la empresa' width={3543} height={3543} className={styles.logo} />
       </header>
     </aside>
   )

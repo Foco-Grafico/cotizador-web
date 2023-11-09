ad523c851a9704605ad8e5592fbfa5eeb615c779
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2023-11-09 14:50:28 -0700

    cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import BatchInfo from '@/ui/batchInfo/batchInfo.component'
+import BatchModal from '@/ui/batchModal/batchModal'
 import { Header } from '@/ui/mainHeader/mainHeader.component'
 
 export default function Home () {
@@ -9,6 +10,7 @@ export default function Home () {
           <Header desarrollo='Colina Perla Residencial' />
         </header>
         <BatchInfo index={1} m2={199} currency='MXN' price={1900000} status='Disponible' location='Húmedo' image='/public/logos/deluxe.png' />
+        <BatchModal index={1} m2={199} currency='MXN' price={1900000} status='Disponible' location='Húmedo' image='/public/logos/deluxe.png' />
       </main>
     </>
   )
---
+++ src/ui/batchModal/batchModal.tsx
@@ -0,0 +1,108 @@
+'use client'
+import { IconArrowSquare } from '@/assets/svgs/icons'
+import { useState } from 'react'
+
+interface Props {
+  status: string
+  m2: number
+  currency: string
+  location: string
+  price: number
+  index: number
+  image: string
+  onClickDownload?: (index: number) => void
+  onClickReserve?: (index: number) => void
+}
+
+export default function BatchModal ({ status, m2, currency, location, price, index, image, onClickDownload, onClickReserve }: Props) {
+  const [open, setOpen] = useState(false)
+
+  const closeModal = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.target === e.currentTarget) {
+      setOpen(false)
+    }
+  }
+
+  const toggleModal = () => {
+    setOpen(prev => !prev)
+  }
+
+  return (
+    <aside>
+      <div className='flex flex-col items-center justify-center w-full h-full bg-white'>
+        <IconArrowSquare className='w-4 h-4' onClick={toggleModal} />
+        <h1 className='text-2xl'>Selecciona tu lote</h1>
+        <p className='text-sm'>Selecciona el lote que más te guste</p>
+
+        <button className='w-1/2 h-12 mt-4 bg-blue-500 rounded-md text-white'>Seleccionar</button>
+      </div>
+      {open && (
+        <main className='fixed inset-0 flex items-end justify-end bg-black bg-opacity-50 backdrop-blur-md' onClick={closeModal}>
+          <div className='animate-slide-in-right bg-white p-5 border-2 border-gray-800 w-1/4 h-full'>
+            <span className='float-right cursor-pointer' onClick={toggleModal}>X</span>
+            <img src={image} alt={location} width={650} height={250} />
+            <article className='grid grid-cols-1 grid-rows-1 grid-flow-row gap-5 w-full py-5'>
+              <div className=' flex flex-row  w-full'>
+                <div className='w-1/3 rounded-lg px-2 font-black'>
+                  <span>STATUS: </span>
+                </div>
+                <div className='w-2/3'>
+                  <span>{status} </span>
+                </div>
+              </div>
+              <div className=' flex flex-col justify-center items-center mx-10 border-[#fac456] border-[1px] rounded-xl' />
+              <div className=' flex flex-row  w-full'>
+                <div className='w-1/3 rounded-lg px-2 font-black'>
+                  <span>UBICACIÓN</span>
+                </div>
+                <div className='w-2/3'>
+                  <span>{location} </span>
+                </div>
+              </div>
+              <div className=' flex flex-col justify-center items-center mx-10 border-[#fac456] border-[1px] rounded-xl' />
+              <div className=' flex flex-row  w-full '>
+                <div className='w-1/3 rounded-lg px-2 font-black'>
+                  <span>M2:</span>
+                </div>
+                <div className=' w-2/3'>
+                  <span>{m2} </span>
+                </div>
+              </div>
+              <div className=' flex flex-col justify-center items-center mx-10 border-[#fac456] border-[1px] rounded-xl' />
+              <div className=' flex flex-row  w-full '>
+                <div className='w-1/3 rounded-lg px-2 font-black'>
+                  <span>PRECIO:</span>
+                </div>
+                <div className=' w-2/3'>
+                  <span>{price.toLocaleString('es-MX', {
+                    style: 'currency',
+                    currency
+                  })}
+                  </span>
+                </div>
+              </div>
+              <div className=' flex flex-col justify-center items-center mx-10 border-[#fac456] border-[1px] rounded-xl' />
+              <div className=' flex flex-row  w-full '>
+                <div className='w-1/3 rounded-lg px-2 font-black'>
+                  <span>MONEDA:</span>
+                </div>
+                <div className='w-2/3'>
+                  <span>{currency} </span>
+                </div>
+              </div>
+              <div className=' flex flex-col justify-center items-center mx-10 border-[#fac456] border-[1px] rounded-xl' />
+            </article>
+            <section className='flex flex-row gap-5'>
+              <button className='bg-[#035a54] font-bold text-lg rounded-xl px-2'>
+                <span className='text-white'>Agregar otro lote</span>
+              </button>
+              <button className='bg-[#035a54] font-bold text-lg rounded-xl px-2'>
+                <span className='text-white'>Descargar cotización</span>
+              </button>
+            </section>
+          </div>
+        </main>
+      )}
+    </aside>
+  )
+}

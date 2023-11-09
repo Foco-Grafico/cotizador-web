16f27722cf0a3da53674ea4f53ce68148d9a56c2
Zemogadrian <124641782+Zemogadrian@users.noreply.github.com>
2023-11-09 14:50:21 -0700

    batchModal

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,10 +18,15 @@ const config: Config = {
         'slide-up': {
           '0%': { transform: 'translateY(100%)', opacity: '0' },
           '100%': { transform: 'translateY(0)', opacity: '1' }
+        },
+        'slide-in-right': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' }
         }
       },
       animation: {
-        'slide-up': 'slide-up 0.4s ease-out'
+        'slide-up': 'slide-up 0.4s ease-out',
+        'slide-in-right': 'slide-in-right 0.4s ease-out'
       }
     }
   },

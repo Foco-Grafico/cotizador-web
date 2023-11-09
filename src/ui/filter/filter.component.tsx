a5320946f1958251667287fa166c6bc2cfbb16a0
JesusOlivas12 <chapo7504@outlook.com>
2023-11-09 12:16:55 -0700

    screenlogin

--- src/ui/filter/filter.component.tsx
+++ src/ui/filter/filter.component.tsx
@@ -1,15 +1,24 @@
 'use client'
+
+import { IconArrowSquare } from '@/assets/svgs/icons'
 import styles from '@/ui/filter/filter.module.css'
 import { useState } from 'react'
 
 export const Filters = () => {
   const [isActive, setIsActive] = useState(false)
 
-  const handleToggle = () => setIsActive(prev => !prev)
+  const handleToggle = () => setIsActive(prev => {
+    const invert = !prev
+    if (invert) {
+      document.body.style.overflow = 'hidden'
+    }
+    return invert
+  })
+
   return (
     <section className=''>
       <button className={styles.filterbutton} onClick={handleToggle}>
-        Filtrar
+        Filtrar <IconArrowSquare className={styles.icon} />
       </button>
       <div className='h-[0.05rem] w-full bg-[#7f7f7f8a] m-[0.50rem]' />
       {isActive && (
